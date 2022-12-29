8bedcceec7f8aaec20ea30c4951715cea721245a
Georg Ehrler <georg.ehrler@s2018.tu-chemnitz.de>
2022-12-29 17:05:17 +0100

    t2 calc done

--- L2_RS_WS2223_Data/P_Interp.m
+++ L2_RS_WS2223_Data/P_Interp.m
@@ -1,6 +1,10 @@
 function out = P_Interp(P_start, P_soll, v, a)
     
 dt = 0.01;
+directionP = P_soll - P_start;
+distanceP = norm(directionP);
+directionP = directionP / distanceP;
+
 
 %Startpunkt
 t0 = 0;
@@ -8,14 +12,19 @@ p0 = P_start;
 
 %Beschleunigungsphase
 t1 = v/a;
-p1 = 0.5 * a * t1^2;
+p1 = p0 + directionP * 0.5 * a * t1^2;
+
 
 %Nullbeschleunigung
-
+p2 = P_soll - directionP * v/a;
+t2 = t1 + (p2 - p1) / v;
 
 %Zielpunkt
 pn = P_soll;
 tn = t2 + v/a;
+
+%time steps
+
 
 
 

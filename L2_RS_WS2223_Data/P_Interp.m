3b12a4f12684c046655d77a3ba7c7d2b36645a81
Georg Ehrler <georg.ehrler@s2018.tu-chemnitz.de>
2023-01-04 19:21:14 +0100

    round values down

--- L2_RS_WS2223_Data/P_Interp.m
+++ L2_RS_WS2223_Data/P_Interp.m
@@ -12,19 +12,29 @@ p0 = P_start;
 
 %Beschleunigungsphase
 t1 = v/a;
+t1 = round(t1,"minusinf");
 p1 = p0 + directionP * 0.5 * a * t1^2;
 
 
 %Nullbeschleunigung
 p2 = P_soll - directionP * v/a;
 t2 = t1 + (p2 - p1) / v;
+t2 = round(t2,"minusinf");
 
 %Zielpunkt
 pn = P_soll;
 tn = t2 + v/a;
+tn = round(tn,"minusinf");
 
 %time steps
-steps = 0:dt:tn;
+noSteps = tn / dt;
+noSteps = round(noSteps,"minusinf");
+P = zeros(noSteps,3);
+
+%interpolating for acceleration phase
+for i=0:dt:t1
+    P(i,:) = 
+end
 
 
 

8807ebc71f68f0a694e1bebe0442f3b6d5bbef3e
Georg Ehrler <georg.ehrler@s2018.tu-chemnitz.de>
2023-01-04 21:01:28 +0100

    trajectory planning works for "normal" distances

--- L2_RS_WS2223_Data/P_2_Main.m
+++ L2_RS_WS2223_Data/P_2_Main.m
@@ -20,9 +20,12 @@ Zwischenschritte = 128;
 P_start = [0.0 0.0 0.0]';
 P_soll = [0.41 0.2595 0.084]';
 
-P = linear_Interpol(P_start, P_soll, Zwischenschritte);
+%P = linear_Interpol(P_start, P_soll, Zwischenschritte);
 
 %% Erweiterung mit Geschw. und Beschleunigung
-v = 1;
-a = 1;
-P = P_Interp(P_start, P_soll, v, a);
+v = 0.2;
+a = 0.5;
+[P,testValue] = P_Interp(P_start, P_soll, v, a);
+
+plot(P(:,1));
+
--- L2_RS_WS2223_Data/P_Interp.m
+++ L2_RS_WS2223_Data/P_Interp.m
@@ -1,9 +1,9 @@
-function out = P_Interp(P_start, P_soll, v, a)
+function [out,testValue] = P_Interp(P_start, P_soll, v, a)
     
 dt = 0.01;
 directionP = P_soll - P_start;
 distanceP = norm(directionP);
-directionP = directionP / distanceP;
+directionP = directionP / norm(directionP);
 
 
 %Startpunkt
@@ -12,37 +12,47 @@ p0 = P_start;
 
 %Beschleunigungsphase
 t1 = v/a;
-t1 = round(t1,"minusinf");
+%t1 = round(t1,TieBreaker="minusinf");
 p1 = p0 + directionP * 0.5 * a * t1^2;
 
 
 %Nullbeschleunigung
-p2 = P_soll - directionP * v/a;
-t2 = t1 + (p2 - p1) / v;
-t2 = round(t2,"minusinf");
+p2 = P_soll - directionP * 0.5 * a * (v/a)^2;
+distA0 = p2 - p1;
+t2 = t1 + norm(distA0) / v;
+%t2 = round(t2,TieBreaker="minusinf");
 
 %Zielpunkt
 pn = P_soll;
 tn = t2 + v/a;
-tn = round(tn,"minusinf");
+%tn = round(tn,TieBreaker="minusinf");
 
 %time steps
 noSteps = tn / dt;
-noSteps = round(noSteps,"minusinf");
+noSteps = round(noSteps,TieBreaker="minusinf");
 P = zeros(noSteps,3);
 
 %interpolating for acceleration phase
-for i=0:dt:t1
-    P(i,:) = 
+currIndex = 1;
+for i = 0:dt:t1
+    P(currIndex,:) = directionP' * 0.5 * a * i^2;
+    currIndex = currIndex + 1;
+end
+
+for i = t1:dt:t2
+    P(currIndex,:) = p1' + directionP' * v * (i - t1);
+    currIndex = currIndex + 1;
+end
+
+for i = t2:dt:tn
+    P(currIndex,:) = p2' + directionP' * v * (i - t2) - directionP' * 0.5 * a * (i - t2)^2;      
+    currIndex = currIndex + 1;
 end
 
 
 
 
 
-
-
-
-
+testValue = norm(directionP);
 out = P;
 end

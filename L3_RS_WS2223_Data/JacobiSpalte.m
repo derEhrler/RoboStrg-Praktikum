6855fa52151ab3286c21e19036cccebdf6e2d8dc
Georg Ehrler <georg.ehrler@outlook.com>
2023-01-30 01:16:30 +0100

    add: functions for calculation of transformation and jacobian matrices, copied and slightly modified from V2

---
+++ L3_RS_WS2223_Data/JacobiSpalte.m
@@ -0,0 +1,12 @@
+function out = JacobiSpalte(T_i,T_tcp)
+%% Bitte vervollständigen 
+% Calculate the collumn of the jacobian for joint i
+
+J_1 = cross(T_i(1:3,3),(T_tcp(1:3,4)-T_i(1:3,4)));
+J_2 = T_i(1:3,3);
+J = [J_1;
+     J_2];
+%J = J_1;
+
+out = J;
+end

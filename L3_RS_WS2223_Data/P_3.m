874844fc9ff24232cfd040c83c351ea1fd98d783
Georg Ehrler <georg.ehrler@outlook.com>
2023-01-30 01:18:28 +0100

    add: calculation of transformation matrices for links and engines TODO: work out how to implement shift of center of gravity

--- L3_RS_WS2223_Data/P_3.m
+++ L3_RS_WS2223_Data/P_3.m
@@ -7,13 +7,15 @@ D6 = 0.160;
 e2 = 0.0098;
 d6b = D3+D6;
 
-%q=sym('q',[4,1]); % Für die symbolische Berechnung
+% Gelenkwinkel
+q = [180 120 180 40];
 
+%q=sym('q',[4,1]); % Für die symbolische Berechnung
 % DH-Parameter: Roboter
-d(1) = D1;   a(1) = 0;  alpha(1) = pi/2;  theta(1) = q(1);   
-d(2) = 0;    a(2) = D2; alpha(2) = pi;      theta(2) = q(2); 
-d(3) = -e2;  a(3) = 0;  alpha(3) = pi/2;    theta(3) = q(3); 
-d(4) = -d6b;  a(4) = 0;   alpha(4) = pi;    theta(4) = q(4); 
+d(1) = D1;      a(1) = 0;   alpha(1) = pi/2;    theta(1) = q(1);   
+d(2) = 0;       a(2) = D2;  alpha(2) = pi;      theta(2) = q(2); 
+d(3) = -e2;     a(3) = 0;   alpha(3) = pi/2;    theta(3) = q(3); 
+d(4) = -d6b;    a(4) = 0;   alpha(4) = pi;      theta(4) = q(4); 
 
 % Position: Schwerpunkt 
 r_Link(:,1) = [     0;     -50;         0]/1000;
@@ -51,16 +53,44 @@ m_Motor(4) = 300/1000;
 
 
 %Transformationsmatrizen fuer Links
+A0_l = [1 0 0 0
+        0 1 0 0
+        0 0 1 0
+        0 0 0 1];
+A1_l = DH(theta(1),d(1),a(1),alpha(1));
+A2_l = DH(theta(2),d(2),a(2),alpha(2));
+A3_l = DH(theta(3),d(3),a(3),alpha(3));
+A4_l = DH(theta(4),d(4),a(4),alpha(4));
+
+T_l = zeros(4,4,4);
+T_l(:,:,1) = A0_l*A1_l;
+T_l(:,:,2) = T_l(:,:,1)*A2_l;
+T_l(:,:,3) = T_l(:,:,2)*A3_l;
+T_l(:,:,4) = T_l(:,:,3)*A4_l;
 
 %Transformationsmatrizen fuer Motoren
+A0_m = [1 0 0 0
+        0 1 0 0
+        0 0 1 0
+        0 0 0 1];
+A1_m = DH(theta(1),d(1),a(1),alpha(1));
+A2_m = DH(theta(2),d(2),a(2),alpha(2));
+A3_m = DH(theta(3),d(3),a(3),alpha(3));
+A4_m = DH(theta(4),d(4),a(4),alpha(4));
 
-
+T_m = zeros(4,4,4);
+T_m(:,:,1) = A0_m*A1_m;
+T_m(:,:,2) = T_m(:,:,1)*A2_m;
+T_m(:,:,3) = T_m(:,:,2)*A3_m;
+T_m(:,:,4) = T_m(:,:,3)*A4_m;
 
 %Erzeugen Sie die einzelnen Jacobi-Matrizen und speichern Sie diese als
 %MATLAB-Funktion ab.
 
 %Jacobi_Matrix Link 1-4:
+J_l = JacobiMatrix(T_l);
 
 %Jacobi_Matrix Motor 1-4:
+J_m = JacobiMatrix(T_m);
 
 
